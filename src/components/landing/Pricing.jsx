d22bcc739a7a2dc94215980036fa0fcc18484e9d
Sean Knowles <sean@skdigital.me>
2019-04-07 11:48:00 +0200

    changed pricing table component, added feature data to pricing tables

---
+++ src/components/landing/Pricing.jsx
@@ -0,0 +1,260 @@
+import React from 'react';
+import { MDBRow, MDBCol, MDBCard, MDBCardBody, MDBBtn } from 'mdbreact';
+
+const Pricing = () => {
+  return (
+    <section className="text-center my-5">
+      <h2 className="h1-responsive font-weight-bold text-center my-5">
+        FREE ACCOUNT FOR LIFE
+      </h2>
+      <p className="grey-text text-center w-responsive mx-auto mb-5">
+        Get a free account for life when you sign up today! We are committed and
+        passionate in bringing the most progressive and modern technology that
+        will provide you the necessary tools to make finding or listing industry
+        opportunities an absolute breeze. Stay connected to your industry and
+        never miss another opportunity.
+      </p>
+      <MDBRow>
+        <MDBCol lg="3" md="12" className="mb-lg-0 mb-4">
+          <MDBCard
+            className="card-image"
+            style={{
+              backgroundImage:
+                'url("https://mdbootstrap.com/img/Photos/Others/pricing-table%20(6).jpg")'
+            }}
+          >
+            <div className="text-white text-center pricing-card d-flex align-items-center rgba-stylish-strong py-3 px-3 rounded">
+              <MDBCardBody>
+                <h5>COMMERCIAL DIVERS & SUPERVISORS</h5>
+                <div className="price pt-0">
+                  <h2 className="number mb-0">FREE</h2>
+                </div>
+                <ul className="striped mb-0">
+                  <li>
+                    <p>
+                      <strong>Unlimited</strong> Dive Logs
+                    </p>
+                  </li>
+                  <li>
+                    <p>
+                      <strong>Unlimited</strong> Job Applications
+                    </p>
+                  </li>
+                  <li>
+                    <p>
+                      <strong>Jobs Board Access</strong> Easily find and apply
+                      for jobs
+                    </p>
+                  </li>
+                  <li>
+                    <p>
+                      <strong>Mobile App</strong> Simple to use & free mobile
+                      app for logging career history
+                    </p>
+                  </li>
+                  <li>
+                    <p>
+                      <strong>Profile</strong> Searchable by companies
+                    </p>
+                  </li>
+                  <li>
+                    <p>
+                      <strong>Companies DB</strong> Search our industry database
+                    </p>
+                  </li>
+                  <li>
+                    <p>
+                      <strong>Industry News & Analytics</strong> stay up to date
+                    </p>
+                  </li>
+                </ul>
+                <MDBBtn outline color="white">
+                  Register free
+                </MDBBtn>
+              </MDBCardBody>
+            </div>
+          </MDBCard>
+        </MDBCol>
+        <MDBCol lg="3" md="12" className="mb-lg-0 mb-4">
+          <MDBCard
+            className="card-image"
+            style={{
+              backgroundImage:
+                'url("https://mdbootstrap.com/img/Photos/Others/pricing-table%20(6).jpg")'
+            }}
+          >
+            <div className="text-white text-center pricing-card d-flex align-items-center rgba-stylish-strong py-3 px-3 rounded">
+              <MDBCardBody>
+                <h5>ROV PILOT & SUPERVISORS</h5>
+                <br />
+                <div className="price pt-0">
+                  <h2 className="number mb-0">FREE</h2>
+                </div>
+                <ul className="striped mb-0">
+                  <li>
+                    <p>
+                      <strong>Unlimited</strong> Dive Logs
+                    </p>
+                  </li>
+                  <li>
+                    <p>
+                      <strong>Unlimited</strong> Job Applications
+                    </p>
+                  </li>
+                  <li>
+                    <p>
+                      <strong>Jobs Board Access</strong> Easily find and apply
+                      for jobs
+                    </p>
+                  </li>
+                  <li>
+                    <p>
+                      <strong>Mobile App</strong> Simple to use & free mobile
+                      app for logging career history
+                    </p>
+                  </li>
+                  <li>
+                    <p>
+                      <strong>Profile</strong> Searchable by companies
+                    </p>
+                  </li>
+                  <li>
+                    <p>
+                      <strong>Companies DB</strong> Search our industry database
+                    </p>
+                  </li>
+                  <li>
+                    <p>
+                      <strong>Industry News & Analytics</strong> stay up to date
+                    </p>
+                  </li>
+                </ul>
+                <MDBBtn outline color="white">
+                  Register free
+                </MDBBtn>
+              </MDBCardBody>
+            </div>
+          </MDBCard>
+        </MDBCol>
+        <MDBCol lg="3" md="12" className="mb-lg-0 mb-4">
+          <MDBCard
+            className="card-image"
+            style={{
+              backgroundImage:
+                'url("https://mdbootstrap.com/img/Photos/Others/pricing-table%20(6).jpg")'
+            }}
+          >
+            <div className="text-white text-center pricing-card d-flex align-items-center rgba-stylish-strong py-3 px-3 rounded">
+              <MDBCardBody>
+                <h5>NDT INSPECTORS & COORDINATORS</h5>
+                <div className="price pt-0">
+                  <h2 className="number mb-0">FREE</h2>
+                </div>
+                <ul className="striped mb-0">
+                  <li>
+                    <p>
+                      <strong>Unlimited</strong> Inspection Logs
+                    </p>
+                  </li>
+                  <li>
+                    <p>
+                      <strong>Unlimited</strong> Job Applications
+                    </p>
+                  </li>
+                  <li>
+                    <p>
+                      <strong>Jobs Board Access</strong> Easily find and apply
+                      for jobs
+                    </p>
+                  </li>
+                  <li>
+                    <p>
+                      <strong>Mobile App</strong> Simple to use & free mobile
+                      app for logging career history
+                    </p>
+                  </li>
+                  <li>
+                    <p>
+                      <strong>Profile</strong> Searchable by companies
+                    </p>
+                  </li>
+                  <li>
+                    <p>
+                      <strong>Companies DB</strong> Search our industry database
+                    </p>
+                  </li>
+                  <li>
+                    <p>
+                      <strong>Industry News & Analytics</strong> stay up to
+                      date.
+                    </p>
+                  </li>
+                </ul>
+                <MDBBtn outline color="white">
+                  Register free
+                </MDBBtn>
+              </MDBCardBody>
+            </div>
+          </MDBCard>
+        </MDBCol>
+        <MDBCol lg="3" md="12" className="mb-lg-0 mb-4">
+          <MDBCard
+            className="card-image"
+            style={{
+              backgroundImage:
+                'url("https://mdbootstrap.com/img/Photos/Others/pricing-table%20(6).jpg")'
+            }}
+          >
+            <div className="text-white text-center pricing-card d-flex align-items-center rgba-stylish-strong py-3 px-3 rounded">
+              <MDBCardBody>
+                <h5>DIVE CONTRACTORS AND CREWING AGENCIES</h5>
+                <div className="price pt-0">
+                  <h2 className="number mb-0">FREE</h2>
+                </div>
+                <ul className="striped mb-0">
+                  <li>
+                    <p>
+                      <strong>Unlimited</strong> Secure Cloud Storage of Company
+                      Dive History
+                    </p>
+                  </li>
+                  <li>
+                    <p>
+                      <strong>Corporate Profile</strong> Verified public profile
+                      & track record that can be used to attract clients or win
+                      projects.
+                    </p>
+                  </li>
+                  <li>
+                    <p>
+                      <strong>Vacancy Listings</strong> List Employment
+                      Opportunities on our Job Boards.
+                    </p>
+                  </li>
+                  <li>
+                    <p>
+                      <strong>Member Database</strong> Search for user profiles
+                      and make direct contact with anyone to offer employment
+                      opportunities.
+                    </p>
+                  </li>
+                  <li>
+                    <p>
+                      <strong>Industry News & Analytics</strong> stay up to
+                      date.
+                    </p>
+                  </li>
+                </ul>
+                <MDBBtn outline color="white">
+                  Register free
+                </MDBBtn>
+              </MDBCardBody>
+            </div>
+          </MDBCard>
+        </MDBCol>
+      </MDBRow>
+    </section>
+  );
+};
+
+export default Pricing;
